aca2f527d5e4e2c35ab6383b08cc95930ffdddb7
NatLM <112110437+NatLM@users.noreply.github.com>
2023-03-21 18:56:18 -0600

    Add files via upload

---
+++ src/main/java/org/uv/paradigmasapi/ParadigmasapiApplication.java
@@ -0,0 +1,13 @@
+package org.uv.paradigmasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadigmasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadigmasapiApplication.class, args);
+	}
+
+}
